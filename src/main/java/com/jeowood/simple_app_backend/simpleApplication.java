b2e5f658910eb47694c53cd1cec00794dceb7af8
JeoWood <mint1944@proton.me>
2024-04-27 15:09:48 +0800

    初始化项目

---
+++ src/main/java/com/jeowood/simple_app_backend/simpleApplication.java
@@ -0,0 +1,11 @@
+package com.jeowood.simple_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class simpleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(simpleApplication.class, args);
+    }
+}
